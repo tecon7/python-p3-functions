ca709557c891c8ca78c449d27a38d40d18e12e4c
Teddy Economou <teddyecon@gmail.com>
2023-02-21 13:23:11 -0500

    completed

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,21 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
 
-def greet(name):
+def greet(name = "Guido"):
     pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
     pass
+    return 45 + 55
 
 def halve(number):
     pass
+    if type(number) == number: return None
+    return number / 2
